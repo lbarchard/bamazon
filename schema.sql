5e3d0b2ad0c7226830b8f0fc9e81c177fc30715d
Les Barchard <les.barchard@gmail.com>
2017-04-16 09:18:56 -0400

    Initial take on schema
    Created database and two tables, one for products and one for departments.

--- schema.sql
+++ schema.sql
@@ -1,1 +1,19 @@
+CREATE DATABASE BamazonDB;
 
+USE BamazonDB;
+
+CREATE TABLE products (
+  item_id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(100) NOT NULL,
+  department_id INT NOT NULL,
+  price DECIMAL(8,2) NOT NULL DEFAULT 0.00,
+  stock_quantity INT NOT NULL DEFAULT 0,
+  product_sales DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (item_id));
+
+CREATE TABLE departments (
+  department_id INT NOT NULL AUTO_INCREMENT,
+  department_name VARCHAR(100) NOT NULL,
+  over_head_costs DECIMAL(10,2) NOT NULL DEFAULT 0.00,
+  total_sales DECIMAL(12,2) NOT NULL DEFAULT 0.00,
+  PRIMARY KEY (department_id));
